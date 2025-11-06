d474785af4038756d5eeb0e8ee3d6021de358f24
judyFariaTecgraf <judyfaria@tecgraf.puc-rio.br>
2025-11-06 11:53:03 -0300

    Implement initial structure for book registration functionality

---
+++ app/acervo.py
@@ -0,0 +1,38 @@
+# NÃO PODE CRIAR CLASSES
+
+# Livro (Título):
+#     ID_Livro (Inteiro, Chave Primária)
+#     Titulo (Texto)
+#     Autor (Texto)
+#     Edicao (Texto)
+
+# Copia (Exemplar Físico):
+#     ID_Copia (Inteiro, Chave Primária)
+#     ID_Livro_Referencia (Chave Estrangeira para Livro)
+#     LocalizacaoFisica (Texto)
+#     Status (Texto: "Disponível", "Emprestado")
+
+_lst_livros = []
+
+_prox_id_livro = 1
+_prox_id_copia =1
+
+def cadastrar_livro(titulo, autor):
+
+    '''
+        Percorrer a lista de livros
+        se o livro a ser adicionado não estiver, adiciona
+        se estiver na lista, retorna que não é possível adicionar, pois já existe
+    '''
+
+    for i, livro in enumerate(_lst_livros):
+
+        if ( livro["ID_Livro"] == )
+
+        novo_livro = {
+            "ID_Livro": _prox_id_livro,
+            "Titulo": titulo,
+            "Autor": autor
+        }
+
+
